9637f4dcce10fddddbec611daa181d22708954b8
Reza Pazan <rezapazan@rocketmail.com>
2022-08-28 21:57:00 +0430

    husky test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-    return (
-        <>
-            <h3 className="text-red-700">Test</h3>
-        </>
-    );
+	return (
+		<>
+			<h3 className="text-red-700">Test</h3>
+		</>
+	);
 };
 
 export default Home;
