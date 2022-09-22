91dbe5a65e3a09fcf759bed6e2e24acc26bdb9d5
saman-taghavi <samantaghavisani@gmail.com>
2022-09-23 00:13:35 +0330

    fix left side

--- components/Isfahan/Beauty/index.tsx
+++ components/Isfahan/Beauty/index.tsx
@@ -16,38 +16,39 @@ const IsfBeauty = (/* props: IIsfBeautyProps */) => {
 
 	return (
 		<>
-			<div className="flex w-full flex-col  px-5">
-				<Pattern className="justify-self-center" />
-				<h3 className=" m-1 flex w-28 items-center justify-between  rounded-lg  font-bold text-chamedoon">
+			<div className="flex w-full flex-col ">
+				<Pattern className="justify-self-center px-5" />
+				<h3 className=" m-1 mx-5 flex w-28 items-center justify-between  rounded-lg  font-bold text-chamedoon">
 					<VideoHorizontal variant="Bold" />
 					<p className=" font-IRANSans text-base font-bold text-white ">اصفهان زیبا</p>
 				</h3>
 				{isLoading ? (
 					<Loading />
 				) : data ? (
-					<Swiper
-						modules={[Autoplay]}
-						className="mt-5 w-full"
-						autoplay={{ delay: 3000 }}
-						slidesPerView={1.9}
-						spaceBetween={20}
-						centeredSlides={false}
-						loop={true}
-						// onLoopFix={() => console.log("loop fix")}
-						// onSlideChange={() => console.log("slide change")}
-						// onSwiper={(swiper) => console.log(swiper)}
-					>
-						{data.map(
-							(item) =>
-								item.link && (
-									<SwiperSlide
-										className="relative flex  aspect-square  items-center justify-center rounded-lg shadow-lg [&>*]:rounded-lg "
-										key={item.id}>
-										<ISFVideoPlayer image={item.image} src={item.link} />
-									</SwiperSlide>
-								),
-						)}
-					</Swiper>
+					<div className="mt-5 w-full pr-5">
+						<Swiper
+							modules={[Autoplay]}
+							autoplay={{ delay: 3000 }}
+							slidesPerView={1.7}
+							spaceBetween={20}
+							centeredSlides={false}
+							loop={true}
+							// onLoopFix={() => console.log("loop fix")}
+							// onSlideChange={() => console.log("slide change")}
+							// onSwiper={(swiper) => console.log(swiper)}
+						>
+							{data.map(
+								(item) =>
+									item.link && (
+										<SwiperSlide
+											className="relative flex  aspect-square  items-center justify-center rounded-lg shadow-lg [&>*]:rounded-lg "
+											key={item.id}>
+											<ISFVideoPlayer image={item.image} src={item.link} />
+										</SwiperSlide>
+									),
+							)}
+						</Swiper>
+					</div>
 				) : (
 					"مشکلی پیش آمده است"
 				)}
