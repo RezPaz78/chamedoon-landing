0b7ac78d277875d7fed0627862b1e79d9a901978
saman-taghavi <samantaghavisani@gmail.com>
2022-09-23 10:09:31 +0330

    how to set breakpoints for resposive slider

--- components/Slider/index.tsx
+++ components/Slider/index.tsx
@@ -40,6 +40,23 @@ const Slider = (/* props: ISliderProps */) => {
 				<Loading />
 			) : data ? (
 				<Swiper
+					// breakpoints={{
+					// 	// when window width is >= 320px
+					// 	320: {
+					// 		slidesPerView: 2,
+					// 		spaceBetween: 20,
+					// 	},
+					// 	// when window width is >= 480px
+					// 	480: {
+					// 		slidesPerView: 3,
+					// 		spaceBetween: 30,
+					// 	},
+					// 	// when window width is >= 640px
+					// 	640: {
+					// 		slidesPerView: 4,
+					// 		spaceBetween: 40,
+					// 	},
+					// }}
 					modules={[Autoplay]}
 					className="w-full "
 					autoplay={{ delay: 3000 }}
